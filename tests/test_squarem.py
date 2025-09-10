993ec9a709fc0d1c9bdf15150d3ff2e3c86b3810
esbenscriver <esbenscriver@gmail.com>
2025-09-10 10:03:24 +0200

    test_squarem.py

--- tests/test_squarem.py
+++ tests/test_squarem.py
@@ -2,7 +2,6 @@ import jax
 import jax.numpy as jnp
 from jax import random
 
-from jaxopt import AndersonAcceleration, FixedPointIteration
 from squarem import SquaremAcceleration
 
 import pytest
@@ -11,22 +10,9 @@ import pytest
 jax.config.update("jax_enable_x64", True)
 
 
-def choose_solver(solver, fun):
-    if solver == "None":
-        return FixedPointIteration(fixed_point_fun=fun)
-    elif solver == "Anderson":
-        return AndersonAcceleration(fixed_point_fun=fun)
-    elif solver == "SQUAREM":
-        return SquaremAcceleration(fixed_point_fun=fun)
-    else:
-        raise ValueError(f"Unknown solver: {solver}")
-
-
 @pytest.mark.parametrize(
     "N, accelerator",
     [
-        (1000, "None"),
-        (1000, "Anderson"),
         (1000, "SQUAREM"),
     ],
 )
@@ -38,7 +24,7 @@ def test_solver(N: int, accelerator: str):
         y = a + x @ b
         return y
 
-    fxp = choose_solver(accelerator, fun)
+    fxp = SquaremAcceleration(fixed_point_fun=fun)
     result = fxp.run(jnp.zeros_like(a))
 
     assert jnp.allclose(result.params, fun(result.params)), (
